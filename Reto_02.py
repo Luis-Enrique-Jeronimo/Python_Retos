bc2820e833dba4877f4e13364b6499d0251a60eb
luis-enrique-jeronimo <jeronimoluis12@gmail.com>
2026-07-18 12:05:01 -0600

    Reto 2 Completo

--- Reto_02.py
+++ Reto_02.py
@@ -1,10 +1,18 @@
 """/*
- * Escribe una función que reciba dos palabras (String) y retorne
- * verdadero o falso (Bool) según sean o no anagramas.
- * - Un Anagrama consiste en formar una palabra reordenando TODAS
- *   las letras de otra palabra inicial.
- * - NO hace falta comprobar que ambas palabras existan.
- * - Dos palabras exactamente iguales no son anagrama.
- */
-"""
+ * Crea una única función (importante que sólo sea una) que sea capaz
+ * de calcular y retornar el área de un polígono.
+ * - La función recibirá por parámetro sólo UN polígono a la vez.
+ * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ * - Imprime el cálculo del área de un polígono de cada tipo.
+ */"""
 
+
+def area_poligono(ladoA, ladoB):
+    triangulo = ladoA * ladoB / 2
+    poligono = ladoA * ladoB
+
+    print(f"El área del Triangulo es: {triangulo}")
+    print(f"El área del Cuadrado es: {poligono}")
+    print(f"El área del Rectángulo es: {poligono}")
+
+area_poligono(4,30)
