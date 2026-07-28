10e34ba23a63f8e9d0eb4b17f2cbf161dfaedb9e
luis-enrique-jeronimo <jeronimoluis12@gmail.com>
2026-07-28 12:44:47 -0600

    Reto 4 INCOMPLETO

---
+++ Reto_04.py
@@ -0,0 +1,16 @@
+"""/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */"""
+
+num1 = input("Ingresa un número: ")
+
+def primos():
+    resultado = l
+
+    if :
+        print("Es Primo")
+    else:
+        print("Es N")
+    l
+
