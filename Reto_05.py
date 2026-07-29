d36cdb2cc449a16dfbd6b3bf8d638daac892b8dd
EnriqueJeronimo <jeronimoluis12@gmail.com>
2026-07-28 19:38:00 -0600

    Retos Incompletos

--- Reto_05.py
+++ Reto_05.py
@@ -1,4 +1,27 @@
 """/*
  * Crea un programa se encargue de transformar un número
- * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ * entero a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
  */"""
+
+
+
+def convertidor_binario():
+
+    dividendo = int(input("Ingresa cualquier número entero: "))
+
+    cociente = dividendo / 2
+    residuo = dividendo % 2
+    
+
+    while cociente > 0:
+
+        dividento = cociente
+
+        cociente = dividendo / 2
+        residuo = dividendo % 2
+
+        print(cociente)
+        print(residuo)
+
+
+convertidor_binario()
